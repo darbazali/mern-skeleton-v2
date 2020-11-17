e1ae69fc968b5a9d14ad4fda2abfad3e164c76ea
Darbaz Ali <darbaz.me@gmail.com>
2020-11-17 14:19:48 +0300

    create user model

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,20 @@ console.clear()
 import mongoose from 'mongoose'
 import app from './express.js'
 import config from '../config/config.js'
-// TODO: setup connection to db
 
+// TODO: setup connection to db
+mongoose.Promise = global.Promise
+mongoose
+  .connect(config.mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log(`MongoDB connected at ${config.mongoUri}`))
+  .catch((err) => console.log(err))
+
+// listen for requrests at port 5000
 app.listen(config.port, () => {
   console.log(`Server is runing on port ${config.port}`)
 })
